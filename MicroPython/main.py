6ca1bd9923ffe61d59e7b268bffb5667dc28a419
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-11-15 18:29:08 +0000

    py vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,38 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Liya G
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variables 
+sprite_xy_coordinate = 0
+turnSprite = 0
+set_pixel = None
+
+# setup
+display.clear()
+display.show(Image.GIRAFFE)
+sleep(1000)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # setup
+        display.clear()
+        turnSprite = 0
+
+        while turnSprite <= 3:
+            
+            sprite_xy_coordinate = 0
+            while sprite_xy_coordinate <= 4:
+                sleep(500)
+
+                display.set_pixel(turnSprite, sprite_xy_coordinate, 9)
+                sleep(500)
+                display.clear()
+
+                sprite_xy_coordinate += 1
+            turnSprite += 1
